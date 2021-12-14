40fc737673c098114d8b9d7cd614b7c47fc0885e
Palme412 <jakepalmer412@gmail.com>
2021-12-14 16:06:07 -0600

    deleted comments

--- practice.js
+++ practice.js
@@ -1,7 +1,6 @@
-/* Load the HTTP library */
+
 var http = require("http");
 
-/* Create an HTTP server to handle responses */
 
 http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const { Genre, Rap, Country, Pop, Rock } = require("./models");
 const SECRET_SESSION = process.env.SECRET_SESSION;
 console.log(SECRET_SESSION);
 
-// code for Spotify API
 const axios = require('axios');
 const { generateKey } = require('crypto');
 let buff = new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
@@ -29,9 +28,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 app.use(session({
-  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
-  resave: false,             // Save the session even if it's modified, make this false
-  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
 }));
 
 app.use(flash());
@@ -50,13 +49,11 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-// Add this above /auth controllers
 app.get('/views/profile', isLoggedIn, (req, res) => {
   const { id, name, email } = req.user.get();
   res.render('profile', { id, name, email });
 });
 
-// controllers
 app.use('/auth', require('./controllers/auth'));
 app.use('/rap', require('./controllers/rap'));
 app.use('/rock', require('./controllers/rock'));
